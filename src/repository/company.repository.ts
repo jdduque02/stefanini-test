f15798b293402ad83a5e4851a8cde27c4613b176
jdduque07 <jdduqueg07@gmail.com>
2026-01-07 22:14:47 -0500

    feat: :sparkles: cambiando el nombre del repositorio

---
+++ src/repository/company.repository.ts
@@ -0,0 +1,69 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { promises as fs } from 'node:fs';
+import { join } from 'node:path';
+import { v4 as uuidv4 } from 'uuid';
+import { CompanyRepositoryPort } from '../domain/interfaces/company-repository.port';
+import { Company } from '../domain/entities/company.entity';
+import { CreateCompanyDto } from '../domain/interfaces/create-company.dto';
+
+
+@Injectable()
+export class JsonCompanyRepository implements CompanyRepositoryPort {
+  private readonly filePath: string;
+
+  constructor() {
+    this.filePath = join(__dirname, '..', 'db', 'company.json');
+  }
+
+  private async readFile(): Promise<Company[]> {
+    try {
+      const data = await fs.readFile(this.filePath, 'utf-8');
+      if (!data || data?.trim() === '') {
+        return [];
+      }
+      return JSON.parse(data);
+    } catch (error) {
+      Logger.error('Error al leer el archivo JSON:', error.message);
+      throw error;
+    }
+  }
+
+  private async writeFile(companies: Company[]): Promise<void> {
+    await fs.writeFile(
+      this.filePath,
+      JSON.stringify(companies, null, 2),
+      'utf-8',
+    );
+  }
+
+  async findAll(): Promise<Company[]> {
+    return await this.readFile();
+  }
+
+  async create(createCompanyDto: CreateCompanyDto): Promise<Company> {
+    try {
+        const companies = await this.readFile();
+
+        const existingCompany = companies.find(
+        ({ cuit }) => cuit === createCompanyDto.cuit,
+        );
+        if (existingCompany) {
+            throw new Error(
+                `Ya existe una empresa con el CUIT ${createCompanyDto.cuit}`,
+            );
+        }
+        const newCompany: Company = {
+        id: uuidv4(),
+        ...createCompanyDto,
+        };
+
+        companies.push(newCompany);
+        await this.writeFile(companies);
+
+        return newCompany;
+    } catch (error) {
+        Logger.error('Error al crear la empresa:', error.message);
+        throw new Error('Error al crear la empresa');
+    }
+  }
+}
